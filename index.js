bb0b67efbbdee3370496771dac81caa0ec1e4d92
Felipe Rocha <felipemota.rocha@gmail.com>
2020-07-28 22:01:47 -0300

    Added Stripe API integration

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { ApolloServer } = require("apollo-server-express");
 const typeDefs = require("./graphql/typeDefs/index");
 const resolvers = require("./graphql/resolvers/index");
 const auth = require("./middlewares/auth/auth.middleware");
+const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 require("./db/mongoose");
 
 const app = express();
@@ -16,6 +18,32 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app, auth });
+
+app.use(cors());
+
+app.get("/product-payment", async ({ query: { amount } }, res) => {
+	const session = await stripe.checkout.sessions.create({
+		payment_method_types: ["card"],
+		line_items: [
+			{
+				price_data: {
+					currency: "usd",
+					unit_amount: amount,
+					product_data: {
+						name: "ROCK-CLOTHING",
+					},
+				},
+				quantity: 1,
+			},
+		],
+		mode: "payment",
+		success_url: `http://localhost:3000/payment-successful?amount=${
+			amount / 100
+		}`,
+		cancel_url: "http://localhost:3000/payment-failure",
+	});
+	res.status(200).send({ sessionId: session.id });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
